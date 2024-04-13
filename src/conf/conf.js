f7ea71261dca207e20f47e51f2fc7f66a454a88c
Rohit <singhrohit51967@gmail.com>
2024-04-13 12:07:00 +0530

    setting up conf file

---
+++ src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDBid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+}
+
+export default conf

0cd6585c03c4c38b251268ab858cd617f3d300e6
Rohit <singhrohit51967@gmail.com>
2024-04-16 16:39:57 +0530

    Adding user detail components

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,21 +1,19 @@
+import React from "react";
 import { useSelector } from "react-redux";
+
 function UserProfile() {
     const userData = useSelector((state) => state.auth.userData);
-    const eventDetail = useSelector((state) => state.auth.eventData)
-    console.log(eventDetail)
+    const eventDetail = useSelector((state) => state.auth.eventData);
+
     return (
-        <div>{<ul className='flex ml-auto'>
-            {
-
-                eventDetail?.map((item) => (
-                    <li key={item.eventName}>
-                        <h1>{`${item.eventName} : ${item.NoOfTickets}`}</h1>
-                    </li>
-                ))
-
-
-            }</ul>}</div>
-    )
+        <div className="bg-gray-100 p-6 rounded-lg shadow-md w-64 mx-auto">
+            <div className="text-center">
+                <h2 className="text-2xl font-bold text-gray-800">{userData.name}</h2>
+                <p className="text-gray-600">{userData.email}</p>
+            </div>
+            {/* Add additional user details or components here */}
+        </div>
+    );
 }
 
-export default UserProfile
+export default UserProfile;
